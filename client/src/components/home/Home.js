18d2d014329636a4a06f58866998ca08fe8bdb27
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-09-08 21:50:25 +0300

    added home page  style

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
+import "./home.css"
 
 const Home = () => {
 
   return (
-    <p>
-      you are in Home component
-    </p>
-  )
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://c0.wallpaperflare.com/preview/447/552/983/ecommerce-online-shop-euro.jpg"
+      alt=""
+    />
+
+    <Carousel.Caption>
+      <h1 style={{color:"black"}}>SHOP WITH US</h1>
+      <p style={{color:"black"}}> shopping is easier than ever </p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>  )
 }
 
 export default Home;
