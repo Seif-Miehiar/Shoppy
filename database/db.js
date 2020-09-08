4fb9445d240eaafb988f8b5e2bc2f7ce96514aa0
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-09-08 20:44:21 +0300

    added dotenv package

--- database/db.js
+++ database/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://shop-user:testing123@shop-db.yxccd.mongodb.net/users?retryWrites=true&w=majority', {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@shop-db.yxccd.mongodb.net/users?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require('dotenv').config()
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const connectDB = require('./database/db')
